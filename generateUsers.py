296b695e94da16bab6970c544c6544f18120594d
Omer Katz <omerktz@gmail.com>
2017-04-25 17:23:06 +0300

    update_users

--- generateUsers.py
+++ generateUsers.py
@@ -2,5 +2,5 @@ import sys
 with open('web/users.txt','w') as f:
 	for i in range(1,int(sys.argv[1])+1):
 		print '\r'+str(i).zfill(len(sys.argv[1]))+'/'+sys.argv[1],
-		f.write('user0'+str(i).zfill(9)+':pass0'+str(i).zfill(9)+'\n')
+		f.write('user0'+str(i).zfill(6)+':pass0'+str(i).zfill(6)+'\n')
 print ''
